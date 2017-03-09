583bb992eca08b43749024309fcc967d02821429
Victor LE DANTEC <victor.le-dantec@epitech.eu>
2017-03-09 13:21:04 +0100

    Changes have been done

--- my_strncmp.c
+++ my_strncmp.c
@@ -5,7 +5,7 @@
 ** Login   <victor.le-dantec@epitech.eu>
 ** 
 ** Started on  Fri Jan  6 21:21:48 2017 Victor LE DANTEC
-** Last update Fri Jan  6 21:33:22 2017 Victor LE DANTEC
+** Last update Thu Mar  9 13:20:57 2017 Victor LE DANTEC
 */
 
 #include <stdlib.h>
@@ -17,7 +17,7 @@ ssize_t		my_strncmp(char *str1, char *str2, size_t n)
   size_t	j;
 
   j = 0;
-  while (str2[j] >= '\0' && str1[j] >= '\0' && str1[j] == str2[j] && n > 0)
+  while (str2[j] != '\0' && str1[j] != '\0' && str1[j] == str2[j] && n > 0)
     {
       j++;
       n--;
